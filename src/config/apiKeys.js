df1c703ea732e76329eff0d757cd89ec547af13a
Fodor Tamas <fodor.tamas2005@gmail.com>
2025-12-09 08:09:43 +0100

    removed api keys for safety reasons

--- src/config/apiKeys.js
+++ src/config/apiKeys.js
@@ -2,7 +2,7 @@
 // Remember to keep this file secure and out of public version control in a real project.
 
 //export const alphaVantageApiKey = '4FOEYF3LTE8IGYRO';
-export const newsApiKey = 'ce2fc084c6624d0badb62d69f5b5eea2';
-export const geminiApiKey = 'AIzaSyBNJrm9z6n1sAcHFOn3OTDM5PzdXvPH1vk';
-export const twelveDataApiKey = '236e0c9adb32410bb4351702c18b874f';
+export const newsApiKey = '';
+export const geminiApiKey = '';
+export const twelveDataApiKey = '';
 
